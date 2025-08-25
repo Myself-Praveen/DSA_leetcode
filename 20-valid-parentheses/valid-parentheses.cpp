a4fce9501e49164e458688c0ad5038a65d84e9be
Myself-Praveen <praveen104685@gmail.com>
2025-08-25 21:21:51 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (14.97%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        if(s.size()%2!=0)
+            return false;
+        else{
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+                st.push(s[i]);
+            else{
+                if(st.size()==0)
+                    return false;
+                else
+                    if((st.top()=='(' && s[i]==')')||(st.top()=='{' && s[i]=='}')||(st.top()=='['&&s[i]==']'))
+                        st.pop();
+                    else return false;
+            }
+        }
+        if(st.size()==0) return true;
+        else return false;
+
+    }
+
+        
+    }
+};
