361d16b1ca25bb31830030367a17ce276910335b
Myself-Praveen <praveen104685@gmail.com>
2025-09-13 23:43:28 +0530

    Time: 318 ms (5.05%) | Memory: 23.8 MB (19.80%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -10,29 +10,24 @@
  * };
  */
 class Solution {
-private:    
-    int findHeight(TreeNode* root){
-        if(root == NULL) return 0;
-        int left = findHeight(root->left);
-        int right = findHeight(root->right);
-
-        return 1 + max(left, right);
-    }
-    void findDiameter(TreeNode* root, int &maxDiameter){
-        if(root == NULL) return;
-
-        int leftHeight = findHeight(root->left);
-        int rightHeight = findHeight(root->right);
-
-        maxDiameter = max(maxDiameter, leftHeight + rightHeight);
-        findDiameter(root->left, maxDiameter);
-        findDiameter(root->right, maxDiameter);
-
-    }
 public:
+    int maxdia=0;
+
+    int height(TreeNode* root){
+        if(root==NULL)return 0;
+        int left=height(root->left);
+        int right=height(root->right);
+        return 1+max(left,right);
+    }
     int diameterOfBinaryTree(TreeNode* root) {
-        int maxDiameter = 0;
-        findDiameter(root, maxDiameter);
-        return maxDiameter;
+        if(root==NULL) return 0;
+        
+        int dia=height(root->left)+height(root->right);
+        maxdia=max(maxdia,dia);
+        diameterOfBinaryTree(root->left);
+        diameterOfBinaryTree(root->right);
+        return maxdia;
+
+        
     }
 };
