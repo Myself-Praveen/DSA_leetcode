2ef2ad913e5abe524332e8f24386d1ecc58309bb
Myself-Praveen <praveen104685@gmail.com>
2025-10-29 16:22:53 +0530

    Time: 3 ms (27.01%) | Memory: 7.8 MB (50.15%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string add;
+        for(int i=0;i<address.size();i++){
+            if(address[i]!='.'){
+                add.push_back(address[i]);
+            }
+            else{
+                add.push_back('[');
+                add.push_back('.');
+                add.push_back(']');
+            }
+        }
+        return add;
+    }
+};
