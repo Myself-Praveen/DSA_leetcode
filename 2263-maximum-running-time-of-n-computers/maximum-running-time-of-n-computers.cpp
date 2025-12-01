9c971673b0c8d9293ba8cdcbd9c27141b0453627
Myself-Praveen <praveen104685@gmail.com>
2025-12-01 23:25:56 +0530

    Time: 15 ms (92.99%) | Memory: 59.9 MB (15.29%) - LeetSync

---
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool check(int n, vector<int>& batteries, long long mid) {
+    long long sum = 0;
+    for (long long b : batteries)
+        sum += min(b, mid);
+
+    return sum >= n * mid;
+}
+
+    long long maxRunTime(int n, vector<int>& batteries) {
+        long long tot=0;
+        for(auto it:batteries){
+            tot+=it;
+        }
+        long long l=0;
+        long long r=tot/n;
+        long long res=0;
+        while(l<=r){
+            long long mid=l+(r-l)/2;
+            if(check(n,batteries,mid)){
+                res=mid;
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return res;
+    }
+};
