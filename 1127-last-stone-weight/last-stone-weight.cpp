c7e1c1d056f37c41d2444b0e293361a174fbaa5b
Myself-Praveen <praveen104685@gmail.com>
2025-12-07 03:15:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (76.92%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq;
+        for(auto el:stones){
+            pq.push(el);
+        }
+        while(pq.size()>1){
+            int a=pq.top();
+            pq.pop();
+            int b=pq.top();
+            pq.pop();
+            if(a!=b) pq.push(a-b);
+        }
+        if(pq.size()==1) return pq.top();
+        return 0;
+    }
+};
