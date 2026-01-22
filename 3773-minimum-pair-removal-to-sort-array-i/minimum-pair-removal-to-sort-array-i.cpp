9936cd78b58c73880c0e9f3fe7314ab416f91432
Myself-Praveen <praveen104685@gmail.com>
2026-01-23 01:35:57 +0530

    Time: 0 ms (100.00%) | Memory: 26.4 MB (81.64%) - LeetSync

--- 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
     int minimumPairRemoval(vector<int>& nums) {
-        int ops = 0;
-        
-        // Keep iterating until array is non-decreasing
-        while (true) {
-            bool sorted = true;
-            int n = nums.size();
-            
-            // Check if array is non-decreasing
-            for (int i = 0; i < n - 1; i++) {
-                if (nums[i] > nums[i + 1]) {
-                    sorted = false;
+        int op=0;
+        while(1){
+            int s=0;
+            int n=nums.size();
+            if(n <= 1) return op;
+            for(int i=0;i<n-1;i++){
+                if(nums[i]>nums[i+1]){
+                    s=1;
                     break;
                 }
             }
-            if (sorted) return ops;  // Done if sorted
-            
-            // Find leftmost adjacent pair with minimum sum
-            int minSum = INT_MAX, idx = 0;
-            for (int i = 0; i < n - 1; i++) {
-                int sum = nums[i] + nums[i + 1];
-                if (sum < minSum) {
-                    minSum = sum;
-                    idx = i;
+            if(s==0) return op;
+            int mx=INT_MAX;
+            int idx=0;
+            for(int i=0;i<n-1;i++){
+                int sum=nums[i]+nums[i+1];
+                if(sum<mx){
+                    mx=sum;
+                    idx=i;
                 }
             }
-            
-            // Merge the chosen pair
-            nums[idx] = nums[idx] + nums[idx + 1];
+            nums[idx]+=nums[idx+1];
             nums.erase(nums.begin() + idx + 1);
-            ops++;
+            op++;
         }
     }
 };
