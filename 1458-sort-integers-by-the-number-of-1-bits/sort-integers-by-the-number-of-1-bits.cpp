3677ba812c6f503a1c32cf32bfece185e72e5d46
Myself-Praveen <praveen104685@gmail.com>
2026-02-25 11:42:13 +0530

    Time: 4 ms (32.29%) | Memory: 15.2 MB (14.87%) - LeetSync

---
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int fun(int a) {
+        int b = 0;
+        while (a != 0) {
+            b += a % 2;
+            a /= 2;
+        }
+        return b;
+    }
+    vector<int> sortByBits(vector<int>& arr) {
+        vector<int> res;
+        map<int, vector<int>> mp;
+        for (int i = 0; i < arr.size(); i++) {
+            int a = fun(arr[i]);
+            mp[a].push_back(arr[i]);
+        }
+        for (auto x : mp) {
+            int b = x.second.size();
+            sort(x.second.begin(), x.second.end());
+            for (int i = 0; i < b; i++) {
+                res.push_back(x.second[i]);
+            }
+        }
+        return res;
+    }
+};
