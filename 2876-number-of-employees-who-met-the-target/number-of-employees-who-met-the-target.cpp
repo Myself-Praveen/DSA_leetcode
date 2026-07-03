c72d7c3d6ea9433f8044941f4bf3e0740a6cc3de
Myself-Praveen <praveen104685@gmail.com>
2026-07-03 16:56:57 +0530

    Time: 0 ms (100.00%) | Memory: 24.4 MB (86.73%) - LeetSync

---
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& h, int t) {
+        int x=0;
+        for(int i=0;i<h.size();i++){
+            if(h[i]>=t) x++;
+        }
+
+        return x;
+    }
+};
